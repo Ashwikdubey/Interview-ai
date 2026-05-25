bf4d620d77ba0b41f3cd9b4aa2ca6b78eaea433b
Ashwikdubey <ashwikdubey512@gmail.com>
2026-05-25 17:41:37 +0530

    updated axios base url

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const interviewRouter=require("./routes/interview.route");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"http://interview-ai-five-tau.vercel.app",
     credentials:true
 }))
 app.use("/api/auth",authRouter);
